e960b6f9af489cd0ecb295b3e6ad840396319562
lisi <lisi@itcast.cn>
2019-06-23 19:45:53 +0800

    日常测试

---
+++ idea-git-test/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,9 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("测试一下");
+        System.out.println("测试一下");
+        System.out.println("测试一下");
+    }
+}
